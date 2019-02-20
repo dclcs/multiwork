356a072473831e4ec8f8b692e8bd56898efb7877
dclcs <43787815+dclcs@users.noreply.github.com>
2019-02-20 17:18:19 +0800

    add

--- compress2.py
+++ compress2.py
@@ -1,21 +1,21 @@
+# -*- coding: utf-8 -*-
 from functions import *
 
 
-
-
-LZ77_WIN_SIZE = 32   #   8个码子
-
+LZ77_WIN_SIZE = 8   #   8个码子 
+MAX_BIT = max_bit(LZ77_WIN_SIZE)
 data = read_from_origin()
-
-sdata = split_data(data)
-
-# 
-
+# data = "0001001111010011100000110010001111000010111000110011001100010011110100111000001100100011110000101110001100110011"
+print("data length :", len(data))
+sdata = split_data(data) 
 win = ["None" for i in range(LZ77_WIN_SIZE)]
+NUM_DICTS = init_convert_dic(LZ77_WIN_SIZE)
 
 # cwin = sdata[s_idx : s_idx + LZ77_WIN_SIZE] 
-
 def get_table(win,start):
+    '''
+        在滑动窗口中，寻找与第一个码子相同的索引的列表
+    '''
     # len = LZ77_WIN_SIZE
     s = 0
     ll = []
@@ -24,42 +24,6 @@ def get_table(win,start):
           ll.append(s)
         s = s + 1
     return ll  
-
-def convert(num):
-    if num == 1:
-        return "001"
-    elif num == 2:
-        return "010"
-    elif num == 3:
-        return "011"
-    elif num == 4:
-        return "100"
-    elif num == 5:
-        return "101"
-    elif num == 6:
-        return "110"
-    elif num == 7:
-        return "111"
-    elif num == 0:
-        return "000"
-
-def convert_l(num):
-    if num == 1:
-        return "001"
-    elif num == 2:
-        return "010"
-    elif num == 3:
-        return "011"
-    elif num == 4:
-        return "100"
-    elif num == 5:
-        return "101"
-    elif num == 6:
-        return "110"
-    elif num == 7:
-        return "111"
-    elif num == 8:
-        return "000"
 
 def compare_win(cwin, win, s_idx):
     # print("win:",win)
@@ -96,13 +60,42 @@ def compare_win(cwin, win, s_idx):
                     return start, [s_idx, ii, opos, c], True
             opos = opos - 1
 
+
+def LZ77_decompress(data):
+    
+    _length = len(data)
+    de_data = ""
+    for i in range(_length):
+        # print("#Epoch ", str(i))
+        # print("Data is ", data[i])   
+        # print("WIN is ", win)
+        length = len(data[i])           
+        if length == 8:
+            de_data = de_data + data[i]
+            win[0 : LZ77_WIN_SIZE - 1] = win[1:]
+            win[LZ77_WIN_SIZE - 1] = data[i]
+        else:
+            # print(MAX_BIT)
+            ii = de_convert_num(NUM_DICTS,data[i][0:MAX_BIT])
+            ll = de_convert_num(NUM_DICTS,data[i][MAX_BIT:2 * MAX_BIT])
+            da = data[i][2 * MAX_BIT :]
+            # print("ii is ", ii)
+            # print("ll is ", ll)
+            rr = ""
+            for idx in range(ll):
+                rr = rr + win[ii + idx]
+            rr = rr + data[i][2 * MAX_BIT:]
+            # print(rr)
+            de_data = de_data + rr
+    return de_data
+
 def LZ77_compress(data):
     length = len(data)
     compress_data = []
     s_idx = 0   # 记录处理原始数据的位置
     while s_idx <= length - 1:
         # print("### epoch idx:",s_idx)
-        if LZ77_WIN_SIZE - 1 - s_idx < 0:
+        if LZ77_WIN_SIZE - 1 - s_idx <= 0:
             cwin = sdata[s_idx:]
         else:
             cwin = sdata[s_idx : s_idx + LZ77_WIN_SIZE + 1] 
@@ -113,28 +106,32 @@ def LZ77_compress(data):
             compress_data.append(ch)
         else:
             s_idx = pos[0]
-            # i = convert(pos[1])
-            # l = convert_l(pos[2])
-            i = str(pos[1])
-            l = str(pos[2])
+            i = NUM_DICTS[pos[1]]
+            l = NUM_DICTS[pos[2]]
             compress_data.append(i + l + pos[3])
-        # print("")
-        # print("result,win:",win)
         if len(win) != LZ77_WIN_SIZE:
             exit()
-        # print("compress_data:",compress_data)
     return compress_data
 
 # print(cwin)
 compress_ = LZ77_compress(sdata)
-# print(win)
+print(compress_)
+win = ["None" for i in range(LZ77_WIN_SIZE)]
+pdata = LZ77_decompress(compress_)
+print(pdata==data)
+print(len(pdata))
+print(len(data))
+# print(pdata)
 # print(compress_)
-# s = compare_win(cwin,win)
-
-print(len(compress_))
+# print(compress_)
 temp = ""
 i = 0
 while i < len(compress_):
     temp = temp + compress_[i]
     i = i + 1
-print(len(temp))
+# print(len(temp)/8)
+with open("compress_data.txt", "w") as f:
+    f.write(temp)
+with open("p_data.txt", "w") as f:
+    f.write(pdata)
+# print(temp)
--- functions.py
+++ functions.py
@@ -21,3 +21,32 @@ def split_data(data):
         cell = data[i[0]:i[-1] + 1]
         folds.append(cell)
     return folds
+
+def max_bit(num):
+    return len(bin(num)[2:]) - 1
+
+def convert_num(num,std=64):
+    std_len = max_bit(std)
+    _bin = bin(num)[2:]
+    if len(_bin) < std_len:
+        _diff = std_len - len(_bin)
+        diff = "0" * _diff
+        _bin = diff + _bin
+    return _bin
+    # print(bin(num))
+def init_convert_dic(std=64):
+    dicts = {}
+    for i in range(std):
+        # dicts.update(i=convert_num(i, std))
+        dicts[i] = convert_num(i, std)
+    return dicts
+    
+def de_convert_num(num_dic, value):
+    dic_list = list(num_dic.values())
+
+    return dic_list.index(value)
+
+# dicts = init_convert_dic(8)
+# dic_list = list(dicts.values())
+# print(dic_list.index("000"))
+# print(type(dicts.values()))
